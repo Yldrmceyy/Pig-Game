65c1d386ce600eb3692eb60d36a2d3a3237a7ac8
Ceylan Yıldırım <106755050+Yldrmceyy@users.noreply.github.com>
2023-11-19 13:46:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ btnHold.addEventListener('click',function(){
      document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer];
 
      //2. check if player's score is >=100
-     if(scores[activePlayer]>=20){
+     if(scores[activePlayer]>=50){
       //finish game
       playing=false;
       diceEl.classList.add('hidden');
